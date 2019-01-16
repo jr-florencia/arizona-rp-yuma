3585fe7ab55c0e935064a243bd15a2de6425926b
Artemka076 <artemkamasatka@mail.ru>
2019-01-16 20:25:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2746,12 +2746,12 @@ if (message.content.startsWith("/warn")){
         });
     }
 
-    if (message.content.startsWith('форматирование ролей')){
+    if (message.content.startsWith('/форматирование_ролей')){
         console.log("1")
         if (!message.member.hasPermission("ADMINISTRATOR")) return
-        const args = message.content.slice('форматирование ролей').split(/ +/)
+        const args = message.content.slice('/форматирование_ролей').split(/ +/)
         let iyz = 0;
-        let role = message.guild.roles.find(r => r.name == args[2]);
+        let role = message.guild.roles.find(r => r.name == args[1]);
         let membersWithRole = message.guild.roles.get(role.id).members;
         message.channel.send(`\`Успешно запущено!\``)
         await membersWithRole.forEach(async member => {
